ab3273409ac586c547d01af191c269701288f7a8
raizadaharoon <haroon.raizada@cloudreach.com>
2022-08-10 12:57:39 +0100

    changes

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
     "fmt"
     "net/http"
+"log"
 )
 
 func main() {
-//    http.HandleFunc("/", HelloServer)
+    http.HandleFunc("/", HelloServer)
   //  http.ListenAndServe(":8080", nil)
-if err := http.ListenAndServe(":8080", nil); err != nil {
+if err := http.ListenAndServe("0.0.0.0:8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
